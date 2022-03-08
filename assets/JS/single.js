b8782414dc7898a0026d8250ed692ca882a6859f
troat9824 <troat9824@gmail.com>
2022-03-08 03:37:24 -0600

    error handling

--- assets/JS/single.js
+++ assets/JS/single.js
@@ -1,12 +1,18 @@
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
-
+var repoNameEl = document.querySelector("#repo-name")
 
 var getRepoName = function() {
     var queryString = document.location.search;
     var repoName = queryString.split("=")[1];
-    getRepoIssues(repoName);
-    repoNameEl.textContent = repoName;
+    if (repoName) {
+        repoNameEl.textContent = repoName;
+        getRepoIssues(repoName);
+    } else {
+        document.location.replace("./index.html");
+    }
+    
+    
 }
 
 var getRepoIssues = function(repo) {
@@ -25,7 +31,7 @@ var getRepoIssues = function(repo) {
                 }
             });
         } else {
-            alert("There was a problem with your request!")
+            document.location.replace("./index.html");
         }
     });
 };
